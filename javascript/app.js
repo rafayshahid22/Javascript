a2cd6efc4f0b61723e3eaf1fbe8e1543d4833d88
rafayshahid22 <139442889+rafayshahid22@users.noreply.github.com>
2023-07-24 18:20:49 +0500

    app.js

--- javascript/app.js
+++ javascript/app.js
@@ -1,3 +1,27 @@
+// Assignment Js-1
+
+// Question 01
+
 var r = 3;
 pi = 3.142;
 alert("Area of Circle = " + pi*r*r)
+
+// Question 02
+
+var a = 4;
+var b = 8;
+console.log(a*b);
+console.log(a/b);
+ 
+
+// Question 03
+
+var x = prompt("Enter a number:") ;
+if (x == 2 || x == 3 || x == 5 || x == 7 || x == 11 || x == 13 || x == 17 || x == 19 || x == 23 || x == 27 || x == 29 ){
+    alert("Prime Number");
+}
+else{
+    alert("Not Prime");
+}
+
+// end
